521e8cec46edf6c2684cabd5917dc3377f751432
lerbear <131228495+lerbear@users.noreply.github.com>
2023-08-10 18:10:55 +0800

    Add files via upload

--- themes/simple - 副本/components/Footer.js
+++ themes/simple - 副本/components/Footer.js
@@ -1,5 +1,6 @@
 import BLOG from '@/blog.config'
 import DarkModeButton from '@/components/DarkModeButton'
+import { VercelIcon } from '@/components/VercelIcon'
 
 export const Footer = (props) => {
   const d = new Date()
@@ -11,20 +12,32 @@ export const Footer = (props) => {
     return currentYear
   })()
 
-  return <footer className="relative w-full bg-black px-6 border-t">
-        <DarkModeButton className='text-center pt-4'/>
+  return (
+    <footer className="relative w-full border-t">
+      <DarkModeButton className="text-center pt-4" />
 
-        <div className="text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {BLOG.AUTHOR}. All rights reserved.</div>
-            <div className="md:p-0 text-center md:text-right text-xs">
-                {/* 右侧链接 */}
-                {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
-                {BLOG.BEI_AN && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{BLOG.BEI_AN} </a>)}
-                <span className='no-underline ml-4'>
-                    Powered by
-                    <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> NotionNext {BLOG.VERSION}  </a>
-                </span>
-            </div>
+      <div className="text-gray-500 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+        <div className="text-center">
+          &copy;{`${copyrightDate}`} {BLOG.AUTHOR}. All rights reserved.
         </div>
+        <div className="md:p-0 text-center md:text-right text-xs">
+          {/* 右侧链接 */}
+          {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
+          {BLOG.BEI_AN && (
+            <a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">
+              {BLOG.BEI_AN}
+            </a>
+          )}
+          <span className="no-underline ml-4">
+            Powered by
+            <a href="https://vercel.com" className="hover:underline">
+              {' '}
+              Vercel
+              <VercelIcon className="inline-block w-4 h-4 ml-1" />
+            </a>
+          </span>
+        </div>
+      </div>
     </footer>
+  )
 }
